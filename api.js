7a3029ed8c638842b2616594c70f5ef3e0f8a74c
Hamish Lees <hamishlees6@gmail.com>
2019-06-06 23:32:35 +1000

    Able to get all coder blogs - still not efficient n^2 Big O

--- api.js
+++ api.js
@@ -5,45 +5,62 @@ The way the app is setup at the moment, it will get all blogs, all the time. The
 const axios = require('axios');
 require("dotenv").config(); 
 
+const coder = [
+    '@coderacademy',
+    '@alxdwa',
+    '@wlei6277'
+];
+
 // Get all the blogs written by CoderAcademy
-const getMediumFeed = async () =>  {
-	try {
-        // Worth noting that the rss is converted to json using the below link. If this breaks for whatever reason, there are lots of alternatives
-        const response = await axios.get(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@coderacademy`);
+const getMediumFeed = async (coder) =>  {
+    const blogs = [];
 
-        // If we want to post more data
-        return response.data.items;
+    for (let dev of coder) {
+        try {
+            // Worth noting that the rss is converted to json using the below link. If this breaks for whatever reason, there are lots of alternatives
+            const response = await axios.get(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${dev}`);
 
-        // If we end up going down the route of only posting the URL
-        // return response.data.items[0].guid;
-	} catch (error) {
-		console.error(error);
+            // If we want to post more data
+            blogs.push(response.data.items);
+
+            // If we end up going down the route of only posting the URL
+            // return response.data.items[0].guid;
+        } catch (error) {
+            console.error(error);
+        }
     }
+    
+    return blogs
 }
 
 // Post blog information to DIRECTUS
 // Currently not the most efficient - but gets the job done. It will go through each blog and post them to directus. Directus is set up for only unique URLs, so will reject any duplication. This does however cause an issue, as if a blog is loaded into directus, then deleted, it can never be loaded back in
-const postToDirectus = async (blogs) => {
+const postToDirectus = async (coder) => {
     // Get the current bearer token
     const token = await getBearerToken();
 
-    for (let blog of blogs) {
-        try {
-            const response = await axios.post(`${process.env.URL}/_/items/blog`, {
-                title: blog.title,
-                url: blog.guid,
-                imageurl: blog.thumbnail,
-                content: blog.content
-            },
-            {
-                headers: {
-                    "Authorization": `bearer ${token}`
-                }
-            })
-            .then(response => console.log(response))
-            .catch(error => console.log(error.response.statusText))
-        } catch (error) {
-            console.log(error);
+    // console.log(coder)
+
+    for (let dev of coder) {
+        for (let blog of dev) {
+            try {
+                const response = await axios.post(`${process.env.URL}/_/items/blog`, {
+                    title: blog.title,
+                    url: blog.guid,
+                    imageurl: blog.thumbnail,
+                    content: blog.content,
+                    author: blog.author
+                },
+                {
+                    headers: {
+                        "Authorization": `bearer ${token}`
+                    }
+                })
+                .then(response => console.log(response))
+                .catch(error => console.log(error.response.statusText))
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 }
@@ -63,4 +80,4 @@ const getBearerToken = async () => {
     }
 }
 
-getMediumFeed().then(result => postToDirectus(result));
+getMediumFeed(coder).then(result => postToDirectus(result));
